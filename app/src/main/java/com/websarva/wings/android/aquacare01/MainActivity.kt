eee0ec7f6e59d8fd7ee022740d16bde05ed4f967
HirotoMori1029 <morihiroto1029@gmail.com>
2021-06-24 22:49:15 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/aquacare01/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.websarva.wings.android.aquacare01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
+
