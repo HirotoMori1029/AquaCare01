45e6733cd2a404e919463ad3fb170f9100be2262
HirotoMori1029 <morihiroto1029@gmail.com>
2021-10-08 11:40:33 +0900

    AlarmViewAdapter.ktを微修正

--- app/src/main/java/com/websarva/wings/android/aquacare01/AlarmViewAdapter.kt
+++ app/src/main/java/com/websarva/wings/android/aquacare01/AlarmViewAdapter.kt
@@ -25,14 +25,16 @@ class AlarmViewAdapter(private var alarmListData: MutableList<Alarm>) :
         val alarm = alarmListData[index]
 
         if (alarm.name != "NoData") {
-            holder.taskName.setTextColor(Color.BLUE)
-            holder.taskDateNext.setTextColor(Color.BLACK)
-            holder.taskTimeNext.setTextColor(Color.BLACK)
-            holder.taskNextStr.setTextColor(Color.BLACK)
-            holder.taskDateNextStr.setTextColor(Color.BLACK)
-            holder.taskTimeNextStr.setTextColor(Color.BLACK)
-            holder.taskDeleteBtn.setTextColor(Color.BLACK)
-            holder.taskDeleteBtn.isEnabled = true
+            holder.apply {
+                taskName.setTextColor(Color.BLUE)
+                taskDateNext.setTextColor(Color.BLACK)
+                taskTimeNext.setTextColor(Color.BLACK)
+                taskNextStr.setTextColor(Color.BLACK)
+                taskDateNextStr.setTextColor(Color.BLACK)
+                taskTimeNextStr.setTextColor(Color.BLACK)
+                taskDeleteBtn.setTextColor(Color.BLACK)
+                taskDeleteBtn.isEnabled = true
+            }
             if (alarm.taskState) {
                 holder.alarmIcon.setImageResource(R.drawable.task_waiting_80px)
             } else if (!alarm.taskState) {
@@ -44,17 +46,17 @@ class AlarmViewAdapter(private var alarmListData: MutableList<Alarm>) :
         } else {
             holder.alarmIcon.setImageResource(R.drawable.task_no_data_80px)
         }
-
-
-        holder.taskName.text = alarm.name
-        holder.taskDeleteBtn.setOnClickListener {
-            listener?.onClickBtn(index)
+        holder.apply {
+            taskName.text = alarm.name
+            taskDeleteBtn.setOnClickListener {
+                listener?.onClickBtn(index)
+            }
+            taskDateNext.text = alarm.nextDate
+            taskTimeNext.text = alarm.nextTime
+            taskDatePrev.text = alarm.prevDate
+            taskTimePrev.text = alarm.prevTime
+            taskRepeat.text = alarm.repeat
         }
-        holder.taskDateNext.text = alarm.nextDate
-        holder.taskTimeNext.text = alarm.nextTime
-        holder.taskDatePrev.text = alarm.prevDate
-        holder.taskTimePrev.text = alarm.prevTime
-        holder.taskRepeat.text = alarm.repeat
     }
 
     override fun getItemCount(): Int = alarmListData.size
