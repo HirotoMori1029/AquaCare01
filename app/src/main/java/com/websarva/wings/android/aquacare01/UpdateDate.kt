eeb84586dde410473f95769039b3855cfe27ef06
HirotoMori1029 <morihiroto1029@gmail.com>
2021-10-10 20:45:45 +0900

    unusedなimportを削除

--- app/src/main/java/com/websarva/wings/android/aquacare01/UpdateDate.kt
+++ app/src/main/java/com/websarva/wings/android/aquacare01/UpdateDate.kt
@@ -1,5 +1,4 @@
 package com.websarva.wings.android.aquacare01
 
-import java.util.*
 
 data class UpdateDate (val index:Int, val prevDateStr:String, val prevTimeStr: String, val nextDateStr: String, val nextTimeStr:String, val fireTime: Long)
