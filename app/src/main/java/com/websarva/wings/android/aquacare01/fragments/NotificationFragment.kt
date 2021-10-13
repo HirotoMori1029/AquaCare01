57facda80d76af9e4a8f2a633872f3a1bcba6281
HirotoMori1029 <morihiroto1029@gmail.com>
2021-10-13 13:07:48 +0900

    unused importを削除

--- app/src/main/java/com/websarva/wings/android/aquacare01/fragments/NotificationFragment.kt
+++ app/src/main/java/com/websarva/wings/android/aquacare01/fragments/NotificationFragment.kt
@@ -3,12 +3,10 @@ package com.websarva.wings.android.aquacare01.fragments
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
