e3290da8b8332c5a5d0d8422ff44c98e5333a854
HirotoMori1029 <morihiroto1029@gmail.com>
2021-10-13 23:05:54 +0900

    app_nameを修正

--- app/src/main/java/com/websarva/wings/android/aquacare01/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/aquacare01/MainActivity.kt
@@ -3,7 +3,6 @@ package com.websarva.wings.android.aquacare01
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
